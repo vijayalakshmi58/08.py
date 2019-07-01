26d5eb064072691ad72521c0548ae5041678c42f
T.vijayalakshmi <52314223+vijayalakshmi58@users.noreply.github.com>
2019-07-01 11:48:33 +0530

    Update 08.py

--- 08.py
+++ 08.py
@@ -1,18 +1,8 @@
 # 08.py
-# Python program to find the sum of natural numbers up to n where n is provided by user
-
-# change this value for a different result
-num = 16
-
-# uncomment to take input from the user
-#num = int(input("Enter a number: "))
-
-if num < 0:
-   print("Enter a positive number")
-else:
-   sum = 0
-   # use while loop to iterate un till zero
-   while(num > 0):
-       sum += num
-       num -= 1
-   print("The sum is",sum)
+n = input("Enter Number to calculate sum")
+n = int (n)
+average = 0
+sum = 0
+for num in range(0,n+1,1):
+    sum = sum+num
+print("SUM of first ", n, "numbers is: ", sum )
